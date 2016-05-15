897a3eb5d74b89d6c4e70215b4c5319b78fd1540
SemenovAn <qwerin@tut.by>
2016-05-15 09:22:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ mkdir /home/box/web/logs 2> /dev/null
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/django.wsgi /etc/gunicorn.d/django.wsgi
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/init.d/gunicorn.conf
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/django.wsgi /etc/gunicorn.d/django.wsgi
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/init.d/gunicorn.conf
+#sudo /etc/init.d/gunicorn restart
 
 #kill -HUP /home/box/web/etc/gunicorn.pid 2> /dev/null
 #gunicorn -c etc/gunicorn.conf /etc/gunicorn.d/django.wsgi
