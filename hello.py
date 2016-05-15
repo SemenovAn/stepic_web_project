ffe9f4c7f9d2fd79be79d6bfbf3c24c47f46c516
SemenovAn <qwerin@tut.by>
2016-05-15 11:39:51 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def app(environ, start_response):
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
     resp = environ['QUERY_STRING'].split("&")
-	  resp = [item + "\r\n" for item in resp]
-	  return resp
+    resp = [item + "\r\n" for item in resp]
+    return resp
     
