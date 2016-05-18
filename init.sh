2e43e760a1b08d592f3019ecfb25d4f99d52baf0
SemenovAn <qwerin@tut.by>
2016-05-18 16:17:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,11 @@ git config --global user.email qwerin@tut.by
 mkdir /home/box/web/logs 2> /dev/null
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+cd /home/box/web/ask/
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000 -D
 sudo /etc/init.d/nginx restart
+
+
 
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart﻿﻿
